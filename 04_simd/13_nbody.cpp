bcd266ebefc9ad5c87d185298cd2251d8a27a5ea
WataruKotsugai <65272685+WataruKotsugai@users.noreply.github.com>
2020-05-18 20:25:00 +0900

    Update 13_nbody.cpp

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -12,15 +12,17 @@ int main() {
     fx[i] = fy[i] = 0;
   }
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        double rx = x[i] - x[j];
-        double ry = y[i] - y[j];
-        double r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    __m256 xjvec = _mm256_load_ps(x);
+    __m256 yjvec = _mm256_load_ps(y);
+    __m256 mvec = _mm256_load_ps(m);
+    __m256 xivec = _mm256_set1_ps(x[i]);
+    __m256 yivec = _mm256_set1_ps(y[i]);
+    __m256 rvec = _mm256_rsqrt_ps();
+    __m256 fxvec = _mm256_mul_ps(-rxvec, mvec, rvec);
+    __m256 fyvec = _mm256_mul_ps(-ryvec, mvec, rvec);
+    
+    _mm256_store_pf(fx[i], fxvec);
+    _mm256_store_pf(fy[i], fyvec);
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
