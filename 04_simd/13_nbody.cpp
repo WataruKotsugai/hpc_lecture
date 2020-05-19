2364b7e1e7752356887e7b7f0b6e5099147ba27e
WataruKotsugai <65272685+WataruKotsugai@users.noreply.github.com>
2020-05-19 20:56:35 +0900

    Update 13_nbody.cpp

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -12,17 +12,29 @@ int main() {
     fx[i] = fy[i] = 0;
   }
   for(int i=0; i<N; i++) {
-    __m256 xjvec = _mm256_load_ps(x);
-    __m256 yjvec = _mm256_load_ps(y);
-    __m256 mvec = _mm256_load_ps(m);
     __m256 xivec = _mm256_set1_ps(x[i]);
     __m256 yivec = _mm256_set1_ps(y[i]);
-    __m256 rvec = _mm256_rsqrt_ps();
-    __m256 fxvec = _mm256_mul_ps(-rxvec, mvec, rvec);
-    __m256 fyvec = _mm256_mul_ps(-ryvec, mvec, rvec);
-    float fxj[N-1], fyj[N-1];
-    __
-    for(int j=0; j<N-1; j++){
+    __m256 xjvec = _mm256_load_ps(x);
+    __m256 yjvec = _mm256_load_ps(y);
+    __m256 rxvec = _mm256_sub_ps(xivec, xjvec); // rx = xi - xj
+    __m256 ryvec = _mm256_sub_ps(yivec, yjvec); // ry = yi - yj
+    __m256 r2vec = _mm256_add_ps(_mm256_mul_ps(rxvec, rxvec), _mm256_mul_ps(ryvec, ryvec)); // r2 = rx * rx + ry * ry
+    
+    __m256 zero = _mm256_setzero_ps();
+    __m256 one = _mm256_set1_ps(1);
+    __m256 mask = _mm256_cmp_ps(r2vec, zero, _CMP_GT_OQ);
+    
+    r2vec = _mm256_blendv_ps(one, r2vec, mask); // replace r2vec's element 0 with 1
+    __m256 rvec = _mm256_rsqrt_ps(r2vec); // rvec = 1 / sqrt(r2)
+    __m256 mvec = _mm256_load_ps(m);
+    mvec = _mm256_blendv_ps(zero, mvec, mask);
+    
+    __m256 fxvec = _mm256_mul_ps(-rxvec, _mm256_mul_ps(mvec, _mm256_mul_ps(rvec, _mm256_mul_ps(rvec, rvec))));
+    __m256 fyvec = _mm256_mul_ps(-ryvec, _mm256_mul_ps(mvec, _mm256_mul_ps(rvec, _mm256_mul_ps(rvec, rvec))));
+    float fxj[N], fyj[N];
+    _mm256_store_ps(fxj, fxvec);
+    _mm256_store_ps(fyj, fyvec);
+    for(int j=0; j<N; j++){
       fx[i] += fxj[j];
       fy[i] += fyj[j];
     }
